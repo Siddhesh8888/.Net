3279981a7e1db20f5c10d6bf5e87cdee870cc5e0
dac <siddhantmalik304@gmail.com>
2023-07-26 16:41:50 +0530

    crud operation

---
+++ EmpCrud/Data/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }   
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder
+                    .Entity<Employee>()
+                    .Property(d => d.Dept)
+                    .HasConversion(new EnumToStringConverter<Department>());
+        }
+    
+    }
+}
---
+++ EmpCrud/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public enum Department { HR, SALES, RnD}
+
+    [Index(nameof(Email), IsUnique = true)]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Field is Empty!!!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field is Empty!!!")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Field is Empty!!!")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Field is Empty!!!")]
+        [EnumDataType(typeof(Department))]
+        public Department Dept { get; set; }
+        public DateOnly JoinDate { get; set; }
+        public double Salary { get; set; }
+    }
+}
